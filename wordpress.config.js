3d8f141f15b7dde8440d6fcdd1b5eaf5778e0bc3
Mattia Zanella <mattia_zanella@icloud.com>
2020-10-20 23:56:56 +0200

    Fake endpoint.

--- wordpress.config.js
+++ wordpress.config.js
@@ -1,9 +1,9 @@
 export default {
   protocol: 'http',
-  host: 'mattiazanella.it',
+  host: 'your-amazing-wordpress.com',
   port: false,
-  basePath: '/develop/wordpress-nuxt',
-  path_API: '/wordpress/wp-json/wp/v2',
+  basePath: '/',
+  path_API: 'wp-json/wp/v2',
   /**
    * @description Thanks to IBM, defining URL structure by envs.
    * @see https://www.ibm.com/support/knowledgecenter/SSGMCP_5.2.0/com.ibm.cics.ts.internet.doc/topics/dfhtl_uricomp.html
